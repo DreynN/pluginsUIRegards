88edd5e1fb46c643ac4ce4bf7b147a8ddd8a5984
DreynN <nicolas.vongsanti@thalesgroup.com>
2024-06-13 14:24:59 +0200

    Sur la branche master
    Validation initiale
    
    Modifications qui seront validées :
    	nouveau fichier : nicotest/README.md
    	nouveau fichier : nicotest/package-lock.json
    	nouveau fichier : nicotest/package.json
    	nouveau fichier : nicotest/src/containers/ServiceContainer.jsx
    	nouveau fichier : nicotest/src/i18n/index.js
    	nouveau fichier : nicotest/src/i18n/messages.en.i18n.js
    	nouveau fichier : nicotest/src/i18n/messages.fr.i18n.js
    	nouveau fichier : nicotest/src/main.js
    	nouveau fichier : nicotest/src/plugin-info.json
    	nouveau fichier : nicotest/src/styles/index.js
    	nouveau fichier : nicotest/src/styles/styles.js
    	nouveau fichier : nicotest/target/prod/plugin.js
    	nouveau fichier : nicotest/target/prod/plugin.js.LICENSE.txt
    	nouveau fichier : nicotest/tests/containers/ServiceContainer.test.jsx
    	nouveau fichier : nicotest/tests/i18n/messages.test.js
    	nouveau fichier : nicotest/tests/setup.test.js
    	nouveau fichier : nicotest/webpack.dev.config.js
    	nouveau fichier : nicotest/webpack.prod.config.js
    	nouveau fichier : nicotest/webpack.test.config.js

---
+++ nicotest/tests/containers/ServiceContainer.test.jsx
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2017-2023 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ **/
+import { shallow } from 'enzyme'
+import { assert } from 'chai'
+import { buildTestContext, testSuiteHelpers, uiPluginServiceTestHelpers } from '@regardsoss/tests-helpers'
+import { ServiceContainer } from '../../src/containers/ServiceContainer'
+import styles from '../../src/styles/styles'
+
+const context = buildTestContext(styles)
+
+/**
+ * Test case for {@link ServiceContainer}
+ *
+ * @author nvongsanti
+ */
+describe('[nicotest] Testing ServiceContainer', () => {
+  before(testSuiteHelpers.before)
+  after(testSuiteHelpers.after)
+  it('should exists', () => {
+    assert.isDefined(ServiceContainer)
+  })
+  it('should render self and sub components', () => {
+    const props = {
+      pluginInstanceId: 'stub.id',
+      // stub runtime configuration
+      configuration: uiPluginServiceTestHelpers.buildConfiguration(),
+      // user is optional, let's not provide it here
+      // We also need to mock the methods provided by map dispatch to props, as we import component disconnected from redux
+      getReducePromise: () => new Promise(() => { }),
+      fetchSelectionThroughAction: () => new Promise(() => { }),
+      //component props
+    }
+    shallow(<ServiceContainer {...props} />, { context })
+    // TODO some tests here
+  })
+})
---
+++ nicotest/webpack.dev.config.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2017-2023 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ **/
+const webpackConfigurator = require('@regardsoss/webpack-config-front')
+const webpack = require('webpack')
+
+const PLUGIN_TYPE = 'service'
+const PLUGIN_NAME = 'nicotest'
+
+if (!process.env.GATEWAY_PLUGINS) {
+  console.log('GATEWAY_PLUGINS var must be set')
+  console.log('Syntax: GATEWAY_PLUGINS=http://validation-regards.cloud-espace.si.c-s.fr yarn build:watch')
+  process.abort()
+}
+
+const conf = webpackConfigurator
+  .generateConfig({
+    mode: 'pkg_build_dev',
+    projectContextPath: __dirname,
+  })
+  // Save the plugin into the webpack dev server public folder (dist/dev)
+  .saveDevPlugin(PLUGIN_TYPE, PLUGIN_NAME)
+  .merge({
+    plugins: [
+      new webpack.DefinePlugin({
+        GATEWAY_HOSTNAME: JSON.stringify(process.env.GATEWAY_PLUGINS || 'http://gateway_to_modify_in_your_plugin_webpack_config.cs'),
+      }),
+    ],
+  })
+  .get()
+
+module.exports = conf
